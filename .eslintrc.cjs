008ce37a197789feea8f40bfed7a9c13752fa5cd
ShuZhengZhou <szhoubf@connect.ust.hk>
2023-10-06 11:59:21 +0800

    Update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,8 @@
 /* eslint-env node */
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended",
+  "some-other-config-you-use",
+  "prettier"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
